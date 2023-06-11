b70a846a9aa1eb22ff36224dd0864e814cc338c4
tappei.miyako <miyako0901t@gmail.com>
2023-06-11 22:39:42 +0900

    golang pointer

--- go/pointer/pointer2.go
+++ go/pointer/pointer2.go
@@ -5,39 +5,44 @@ import "fmt"
 func main() {
 	var x = 100
 	fmt.Println("x address:\t", &x)
+    // xアドレス
 
 	var y *int
 	fmt.Println("y value:\t", y)
 	fmt.Println("y address:\t", &y)
+    // y値
+    // yアドレス
+
 	y = &x
 	fmt.Println("y value:\t", y)
 	fmt.Println("y address:\t", &y)
+    // xアドレス
+    // yアドレス
 
-	var z **int
-    fmt.Println("z value:\t", z)
-    fmt.Println("z address:\t", &z)
-    z = &y
-    fmt.Println("z value:\t", z)
-    fmt.Println("z address:\t", &z)
+    fmt.Print("\n\n関数引き渡し\n")
 
-	var a = "alice"
-    fmt.Println("\na address:\t", &a)
-    b := &a
+	var alice = "alice"
+    fmt.Println("\nalice address:\t", &alice)
+    b := &alice
     fmt.Println("b value:\t", b)
     fmt.Println("b address:\t", &b)
+    fmt.Println("b refer:\t", *b)
 
+    // alice変数のポインターbを渡す
 	show(b)
-    fmt.Println(*b)  // bobのつもりがaliceのまま
+    fmt.Println("b refer:\t", *b)  // bobのつもりがaliceのまま
 }
 
+// sに渡されるのはstringのポインター
 func show(s *string) {
     tmp := "bob"
     fmt.Println("tmp address:\t", &tmp)
 
     fmt.Println("s value:\t", s)
     fmt.Println("s address:\t", &s)
+    // 受け取った値は
 
-    *s = tmp  // 上書きのつもり
+    s = &tmp  // 上書きのつもり
     fmt.Println("s value:\t", s)
     fmt.Println("s address:\t", &s)
 }
