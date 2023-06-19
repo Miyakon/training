a7b9bd59878f18fe4e713ca2d97adc4403e8a861
tappei.miyako <miyako0901t@gmail.com>
2023-06-20 07:53:53 +0900

    WIP

---
+++ kotlin/test-project/src/main/kotlin/syntax/MyData.kt
@@ -0,0 +1,11 @@
+package syntax
+
+class MyData(age: Int) {
+
+    var name: String = "";
+    fun test1(): String {
+        var message = "Hello test"
+        println(message)
+        return message
+    }
+}
---
+++ kotlin/test-project/src/test/kotlin/syntax/MyDataTest.kt
@@ -0,0 +1,20 @@
+package syntax
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MyDataTest {
+
+    @Test
+    fun getName() {
+    }
+
+    @Test
+    fun setName() {
+    }
+
+    @Test
+    fun test1() {
+    }
+}
