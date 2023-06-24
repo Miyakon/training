d77fb639fc4de48d7f52791adda1f52f2862f5f7
tappei.miyako <miyako0901t@gmail.com>
2023-06-24 18:20:43 +0900

    WIP

--- kotlin/test-project/src/test/kotlin/syntax/MyDataTest.kt
+++ kotlin/test-project/src/test/kotlin/syntax/MyDataTest.kt
@@ -1,20 +1,16 @@
 package syntax
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
-
+@Nested
 class MyDataTest {
 
     @Test
     fun getName() {
+        val myData = MyData(10)
+        assertEquals(myData.test1(), "Hello test")
     }
-
-    @Test
-    fun setName() {
-    }
-
-    @Test
-    fun test1() {
-    }
+    
 }
