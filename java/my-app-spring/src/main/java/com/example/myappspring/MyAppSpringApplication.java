d2c1af5f8aaea6814516890bd09cbd12d61a1ff0
Tappei Miyako <miyako0901t@gmail.com>
2024-05-03 11:21:24 +0900

    java

---
+++ java/my-app-spring/src/main/java/com/example/myappspring/MyAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.myappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAppSpringApplication.class, args);
+    }
+
+}
