4d5fa536f29b2410697b508dd87cfd5626695a5c
tappei.miyako <miyako0901t@gmail.com>
2024-09-25 20:47:21 +0900

    clean kotlin

--- kotlin/test-project/src/main/kotlin/Main.kt
+++ kotlin/test-project/src/main/kotlin/Main.kt
@@ -1,18 +1,4 @@
 
 
 fun main(args: Array<String>) {
-//    val kanojo = Kanojo()
-//    println(kanojo.stable());
-
-//    val myData = MyData(10);
-//    myData.test1();
-//    myData.name = "Miyako";
-//
-//    ifExpression(true)
-//    ifExpression(false)
 }
-
-//fun ifExpression(condition: Boolean) {
-//    val value: String = if (condition) "Hi, true" else "Hey, false"
-//    println(value)
-//}
